051a2524df385afbdeab69f696e59e773ff4a167
lsmgeb89 <lsmgeb89@gmail.com>
2018-07-29 14:21:14 -0500

    [web][client][server] Connect front end and back end by RESTful API

--- web_server/client/src/NewsPanel/NewsPanel.js
+++ web_server/client/src/NewsPanel/NewsPanel.js
@@ -16,30 +16,16 @@ class NewsPanel extends React.Component {
   }
 
   loadMoreNews() {
-    this.setState({
-      // fake json news data to test front-end UI becaues currently there is no back-end
-      news: [
-        {
-          'url': 'http://us.cnn.com/2017/02/15/politics/andrew-puzder-failed-nomination/index.html',
-          'title': "Inside Andrew Puzder's failed nomination",
-          'description': "In the end, Andrew Puzder had too much baggage -- both personal and professional -- to be confirmed as President Donald Trump's Cabinet.",
-          'source': 'cnn',
-          'urlToImage': 'http://i2.cdn.cnn.com/cnnnext/dam/assets/170215162504-puzder-trump-file-super-tease.jpg',
-          'digest': '3RjuEomJo26O1syZbU7OHA==\n',
-          'reason': 'Recommend'
-        },
-        {
-          'title': 'Zero Motorcycles CTO Abe Askenazi on the future of two-wheeled EVs',
-          'description': "Electric cars and buses have already begun to take over the world, but the motorcycle industry has been much slower to put out all-electric and hybrid models...",
-          'url': "https://techcrunch.com/2017/03/23/zero-motorcycles-cto-abe-askenazi-on-the-future-of-two-wheeled-evs/",
-          'urlToImage': "https://tctechcrunch2011.files.wordpress.com/2017/03/screen-shot-2017-03-23-at-14-04-01.png?w=764&h=400&crop=1",
-          'source': 'techcrunch',
-          'digest': "3RjuEomJo26O1syZbUdOHA==\n",
-          'time': "Today",
-          'reason': "Hot"
-       }
-      ]
-    });
+    const news_url = 'http://' + window.location.hostname + ':3000' + '/news';
+    const request = new Request(news_url, { method:'GET', cache:'no-cache' });
+
+    fetch(request)
+      .then(res => res.json())
+      .then(news_list => {
+        this.setState({
+          news: this.state.news ? this.state.news.concat(news_list): news_list,
+        });
+      });
   }
 
   // because the number of news card to render is dynamic, we need a function to deal with it.
--- web_server/server/app.js
+++ web_server/server/app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var path = require('path');
 
 var indexRouter = require('./routes/index');
+var newsRouter = require('./routes/news');
 
 var app = express();
 
@@ -11,7 +12,16 @@ app.set('view engine', 'jade');
 app.use('/static',
         express.static(path.join(__dirname, '../client/build/static/')));
 
+// TODO: remove this after development is done
+app.all('*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
+
+// register rounters
 app.use('/', indexRouter);
+app.use('/news', newsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
