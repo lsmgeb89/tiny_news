dd9b0df02b19655cc9aaed84241fd1d07c77f7e4
lsmgeb89 <lsmgeb89@gmail.com>
2018-07-29 19:51:16 -0500

    [web][client] Build basic signup page

--- web_server/client/src/index.js
+++ web_server/client/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App/App';
 import LoginPage from './Login/LoginPage';
+import SignUpPage from './SignUp/SignUpPage';
 
 ReactDOM.render(
   <App />,
