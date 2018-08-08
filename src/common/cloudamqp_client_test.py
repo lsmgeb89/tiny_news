5f3be6d52ecf1bbaf69aaa5b3968d2e5d7fe12ec
lsmgeb89 <lsmgeb89@gmail.com>
2018-08-08 01:22:54 -0500

    [backend][common] Clear test queue before testing

--- src/common/cloudamqp_client_test.py
+++ src/common/cloudamqp_client_test.py
@@ -1,12 +1,19 @@
 """Test CloudAMQP client"""
 
+import os
+import sys
+import logging
 from cloudamqp_client import CloudAMQPClient
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'news_pipeline'))
+from queue_helper import clear_queue
 
 CLOUDAMQP_URL = "amqp://hhmlgqql:0wJDR4KJY0Pafg2NkdGwEHLG63oJQxD5@wombat.rmq.cloudamqp.com/hhmlgqql"
 TEST_QUEUE_NAME = "tiny-news-test"
 
 def test_basic():
     """Test CloudAMQP client basically"""
+    clear_queue(CLOUDAMQP_URL, TEST_QUEUE_NAME)
     client = CloudAMQPClient(CLOUDAMQP_URL, TEST_QUEUE_NAME)
 
     sent_msg = {"title": "test news"}
@@ -17,4 +24,5 @@ def test_basic():
     print("test_basic passed!")
 
 if __name__ == "__main__":
+    logging.basicConfig(level=20)
     test_basic()
