faaf366a6a99f9d3b9ea7e75a7d0a272f21c8b1c
lsmgeb89 <lsmgeb89@gmail.com>
2018-08-07 20:07:28 -0500

    Upgrade MongoDB install to Ubuntu 18.04

--- script/setup_env.sh
+++ script/setup_env.sh
@@ -19,11 +19,11 @@ popd
 # install python packages
 sudo apt install -y python3-pip
 sudo -H pip3 install --upgrade pip
-sudo -H pip3 install -r ../src/requirements.txt
+pip3 install --user -r ../src/requirements.txt
 
 # install MongoDB
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD75D9DCB49F368818C72E52529D4
-echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.0.list
+echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.0.list
 sudo apt-get update
 sudo apt-get install -y mongodb-org
 
